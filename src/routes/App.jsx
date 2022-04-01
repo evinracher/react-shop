c47416d91834f854c1b6d3218d09ea3f3e81ff0e
evinracher <eevinley@gmail.com>
2022-04-01 08:21:27 -0500

    12: Solución al reto: componentes y rutas de React Shop

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import ResetPassword from '../containers/ResetPassword';
+import Login from '../pages/Login';
+import ResetPassword from '../pages/ResetPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import '../styles/global.css';
 
 const App = () => {
@@ -15,6 +21,12 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/reset-password" element={<ResetPassword />} />
+          <Route exact path="/send-email" element={<SendEmail />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/account" element={<MyAccount />} />
+          <Route exact path="/sign-up" element={<CreateAccount />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
