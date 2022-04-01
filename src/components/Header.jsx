c00c889617248bdc536a11b8c5721d8887a4c1d2
evinracher <eevinley@gmail.com>
2022-04-01 09:56:32 -0500

    17: Toggle del menú

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Menu from '@components/Menu';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
@@ -6,6 +7,13 @@ import '@styles/Header.scss';
 
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    console.log("Click");
+    setToggle(!toggle);
+  };
+
   return (
     <nav>
       <img src={menu} alt="menu" className="menu" />
@@ -35,15 +43,16 @@ const Header = () => {
 
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
     </nav>
-  )
-}
+  );
+};
 
 export default Header;
