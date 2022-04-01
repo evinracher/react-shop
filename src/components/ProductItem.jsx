9e7d219b8906203664ca9b6191d49b7153085ef6
evinracher <eevinley@gmail.com>
2022-04-01 16:40:39 -0500

    19: Custom Hooks para la tienda

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 import addToCartIcon from "@icons/bt_add_to_cart.svg";
 import '../styles/ProductItem.scss';
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
 	const [cart, setCart] = useState([]);
+
 	const handleClick = () => {
 		const newCart = [...cart];
 		setCart(newCart);
 	};
+
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			{/* <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /> */}
+			<img src={product.images[0]} alt={product.price} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addToCartIcon} alt="add-to-cart" />
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,22 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ProductItem from '../components/ProductItem';
-import axios from 'axios';
 import '../styles/ProductList.scss';
+import useProducts from '../hooks/useProducts';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, []);
+	const products = useProducts(API);
 
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				{products.map(product => (<ProductItem />))}
+				{products.map(({ id, ...product }) => (<ProductItem key={id} product={product} />))}
 			</div>
 		</section>
 	);
