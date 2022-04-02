11617c93ef84104fdcc75d6c50993ee1e1632a61
evinracher <eevinley@gmail.com>
2022-04-01 21:41:53 -0500

    fix: changing configuration

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const state = useAppState();
   return (
     <AppContext.Provider value={state}>
-      <BrowserRouter basename='/react-shop/'>
+      <BrowserRouter basename='/react-shop'>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
